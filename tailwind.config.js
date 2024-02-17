4b19d60a2f33bf9aa4ba4a23c41fa367418ab08a
Sohail Ansari <sohailansari483@gmail.com>
2024-02-18 00:53:57 +0530

    Nav Bar Modifications and crossplateform font fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,18 +6,19 @@ export default {
   ],
   theme: {
     extend: {
-      fontFamily: {
-        kalnia: [
-          "Kalnia", "serif"
-        ],
-        poppins: [
-          "Poppins", "sans-serif"
-        ]
-      },
       colors: {
         'patriarch': '#6A0171',
         'tiffany-blue': '#04AFBC',
         'mystic': '#D64E86',
+        'base-1': '#000000',
+        'base-2': '#1F1F1F',
+        'base-3': '#3D3D3D',
+        'mid-1': '#5C5C5C',
+        'mid-2': '#7A7A7A',
+        'mid-3': '#999999',
+        'contrast-1': '#B8B8B8',
+        'contrast-2': '#D6D6D6',
+        'contrast-3': '#F5F5F5',
       },
     },
   },
